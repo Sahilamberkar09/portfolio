f10741fe54a8f0ffc5d7b97986270cd4846b6645
Sahil Amberkar <sahilamberkar2003@gmail.com>
2025-06-18 22:49:44 +0530

    Ready

--- src/server.js
+++ src/server.js
@@ -3,7 +3,12 @@ import cors from "cors";
 import contactRoute from "./routes/contact.js";
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://portfolio-j69y28dpz-sahilamberkar09s-projects.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/contact", contactRoute);
